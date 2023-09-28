dd567f61d9e459158631429fe45751d2d9471568
CodeFather88 <bulanovigor01@gmail.com>
2023-09-28 22:44:15 +0300

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create.user.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from './users.model';
+
+
+@Controller('users')
+export class UsersController {
+
+    constructor (private usersService: UsersService){}
+    @ApiOperation({summary:"Создание пользователя"})
+    @ApiResponse({status:200, type: User})
+    @Post()
+    create(@Body() UserDto: CreateUserDto){
+        return this.usersService.createUser(UserDto);
+    }
+
+    @ApiOperation({summary:"Получение всех пользователей"})
+    @ApiResponse({status:200, type: [User]})
+    @Get() 
+    getAll(){
+        return this.usersService.getAllUser()
+    }
+
+
+}
